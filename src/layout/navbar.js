d58d959531e756fdad4b260430e2dbc89916c8ab
techcareer <47493790+techcareer@users.noreply.github.com>
2019-06-10 16:26:14 +0300

    tbankReactFront

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -41,7 +41,7 @@ class Navigator extends Component{
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/Register">Register</Nav.Link>
                         <Nav.Link href="/Login">Login</Nav.Link>
-                        <Nav.Link href="/Lines">Add Line</Nav.Link>
+                        <Nav.Link href="/Lines">Add Line2</Nav.Link>
 
                         </Nav>
                         
